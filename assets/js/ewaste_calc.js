0bb1dc291b14985d5b79579b6420c2405aacdf5b
Madi Yeh <79380186+madigyeh@users.noreply.github.com>
2024-03-27 10:17:21 -0400

    Update ewaste_calc.js

--- assets/js/ewaste_calc.js
+++ assets/js/ewaste_calc.js
@@ -1,24 +1,25 @@
  document.getElementById('reset').onclick = function(){
-    document.getElementById('microwave').reset();
-    document.getElementById('kapp').reset();
-    document.getElementById('heatvent').reset();
-    document.getElementById('pc').reset();
-    document.getElementById('vacuum').reset();
-    document.getElementById('TV').reset();
-    document.getElementById('dishwasher').reset();
-    document.getElementById('wash').reset();
-    document.getElementById('dryer').reset();
-    document.getElementById('refrigerator').reset();
-    document.getElementById('aircon_heat').reset();
-    document.getElementById('other_lhe').reset();
-    document.getElementById('lithiumion').reset();
-    document.getElementById('nickel').reset();
-    document.getElementById('alkaline').reset();
-    document.getElementById('mobile').reset();
-    document.getElementById('laptop').reset();
-    document.getElementById('printer').reset();
-    document.getElementById('router').reset();
-    document.getElementById('other_pe').reset();
+    document.getElementById('myform').reset();
+    // document.getElementById('microwave').reset();
+    // document.getElementById('kapp').reset();
+    // document.getElementById('heatvent').reset();
+    // document.getElementById('pc').reset();
+    // document.getElementById('vacuum').reset();
+    // document.getElementById('TV').reset();
+    // document.getElementById('dishwasher').reset();
+    // document.getElementById('wash').reset();
+    // document.getElementById('dryer').reset();
+    // document.getElementById('refrigerator').reset();
+    // document.getElementById('aircon_heat').reset();
+    // document.getElementById('other_lhe').reset();
+    // document.getElementById('lithiumion').reset();
+    // document.getElementById('nickel').reset();
+    // document.getElementById('alkaline').reset();
+    // document.getElementById('mobile').reset();
+    // document.getElementById('laptop').reset();
+    // document.getElementById('printer').reset();
+    // document.getElementById('router').reset();
+    // document.getElementById('other_pe').reset();
  };
 
 function tosubmit() {
