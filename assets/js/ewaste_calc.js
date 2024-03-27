8654b26985223410e8fb55524838ae1b1bc34bcd
Madi Yeh <79380186+madigyeh@users.noreply.github.com>
2024-03-27 09:18:57 -0400

    make reset button

---
+++ assets/js/ewaste_calc.js
@@ -0,0 +1,29 @@
+let submit = document.getElementbyId("submit")
+
+
+submit.addEventListener("click", function() {
+  
+})
+
+document.getElementById('reset').onclick = function(){
+   document.getElementById('microwave').value = 0;
+   document.getElementById('kapp').value = 0;
+   document.getElementById('heatvent').value = 0;
+   document.getElementById('pc').value = 0;
+   document.getElementById('vacuum').value = 0;
+   document.getElementById('TV').value = 0;
+   document.getElementById('dishwasher').value = 0;
+   document.getElementById('wash').value = 0;
+   document.getElementById('dryer').value = 0;
+   document.getElementById('refrigerator').value = 0;
+   document.getElementById('aircon_heat').value = 0;
+   document.getElementById('other_lhe').value = 0;
+   document.getElementById('lithiumion').value = 0;
+   document.getElementById('nickel').value = 0;
+   document.getElementById('alkaline').value = 0;
+   document.getElementById('mobile').value = 0;
+   document.getElementById('laptop').value = 0;
+   document.getElementById('printer').value = 0;
+   document.getElementById('router').value = 0;
+   document.getElementById('other_pe').value = 0;
+};
