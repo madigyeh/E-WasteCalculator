a935947a9eb2e820e46b003c294e3a310b4114ee
Madi Yeh <79380186+madigyeh@users.noreply.github.com>
2024-03-29 10:13:26 -0400

    get numbers to populate

--- assets/js/ewaste_calc.js
+++ assets/js/ewaste_calc.js
@@ -1,4 +1,11 @@
- document.getElementById('reset').onclick = function(){
+const value = document.querySelector("#microwave1");
+const input = document.querySelector("microwave");
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+  value.textContent = event.target.value;
+});
+
+document.getElementById('reset').onclick = function(){
     document.getElementById('microwave').value = 0;
     document.getElementById('microwave1').value = 0;
     document.getElementById('kapp').value = 0;
